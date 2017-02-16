71a94546b1faec866abbed82b38b67a69eeedd28
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2017-02-16 20:15:08 +0100

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	sudoku.py

--- test.py
+++ test.py
@@ -12,6 +12,14 @@ def processColumn(y, num):
         if num in mogelijkNum[x][y]:
             mogelijkNum[x][y].remove(num)
 
+def processBox(x, y, num):
+    boxX = int(x/3)
+    boxY = int(y/3)
+    for x in range(3*boxX, 3*boxX + 3):
+        for y in range(3*boxY, 3*boxY + 3):
+            if num in mogelijkNum[x][y]:
+                mogelijkNum[x][y].remove(num)
+
 def checkMogelijkeNum(sudoku):
     mogelijkNum = []
     for line in sudoku:
@@ -19,7 +27,6 @@ def checkMogelijkeNum(sudoku):
         for num in line:
             if(num == 0):
                 rij.append([1,2,3,4,5,6,7,8,9])
-                //heyDante
             else:
                 rij.append([num])
         mogelijkNum.append(rij)
@@ -27,12 +34,23 @@ def checkMogelijkeNum(sudoku):
 
 sudoku = readSudoku("puzzle1.sudoku")
 mogelijkNum = checkMogelijkeNum(sudoku)
+openIndexCounter = 81
 
-for x in range(0,9):
-    for y in range(0,9):
-        if len(mogelijkNum[x][y])== 1:
-            q = mogelijkNum[x][y][0]
-            mogelijkNum[x][y][0] = "x"
-            sudoku[x][y] = q
-            processRij(x, q)
-print(mogelijkNum)
+while(openIndexCounter>0):
+    for x in range(0,9):
+        for y in range(0,9):
+            if len(mogelijkNum[x][y])== 1:
+                if mogelijkNum[x][y] != 'x':
+                    value = mogelijkNum[x][y][0]
+                    mogelijkNum[x][y][0] = "x"
+                    sudoku[x][y] = value
+                    processRij(x, value)
+                    processColumn(y, value)
+                    processBox(x, y, value)
+                    openIndexCounter -= 1
+for line in mogelijkNum:
+    print(line)
+for line in sudoku:
+    print(line)
+print(openIndexCounter)
+
