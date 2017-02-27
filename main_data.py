b32626a3527efe9789ffea5ebe867f0464c91974
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2017-02-27 18:36:33 +0100

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	sudoku.py

--- main_data.py
+++ main_data.py
@@ -1,6 +1,13 @@
 import hillSudoku
+import time
 import read_data
 
-for amount in [100,200,300,400,500,750,1000]:
-    list = hillSudoku.test(amount)
-    read_data.convert(amount, list)
+#for amount in [100,200,300,400,500,750,1000]:
+#    list = hillSudoku.test(amount)
+#    read_data.convert(amount, list)
+
+start = time.time()
+hillSudoku.run_hillclimber()
+end = time.time()
+elapsed = end - start
+print(elapsed)
