e198c8052a74c5f2cbf11e4d6a098d4ede20794b
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2017-03-02 20:31:53 +0100

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	sudoku.py

--- bigSudokuSolver.py
+++ bigSudokuSolver.py
@@ -1,3 +1,6 @@
+import copy
+import time
+
 def readSudoku(name):
     with open(name, "r") as puzzle:
         return [[int(i) for i in line.split(",")] for line in puzzle]
@@ -37,40 +40,92 @@ def numbers_place(place, sudoku):
 ##doe dit door te checken voor rij, kolom en box
 ##return deze lijst als mogelijk_num
 
-def find_empty(sudoku):
-    ##zoek het (volgende) lege vakje in de sudoku
-    ##check of sudoku succesvol vol is. if so: sudoku_success()
-    ##optimalisatie idee: zoek het vakje met de minste opties en kies die als volgende
-    for y in range (9):
-        for x in range (9):
-            if sudoku[y][x] == 0:
-                return [y,x]
-    return 0
-        ##of zoiets, als alles vol is is de sudoku klaar
-        ## nog even over nadenken
+def find_empty(new_possible_list):
+    minlength = 10
+    place = [10, 10]
+    for y in range(9):
+        for x in range(9):
+            if len(new_possible_list[y][x]) < minlength:
+                minlength = len(new_possible_list[y][x])
+                place = [y, x]
+    if place == [10, 10]:
+        return 0
+    print(minlength)
+    print(place)
+    if minlength == 0:
+        return 1
+    return place
 
-def dfs(sudoku):
-        place = find_empty(sudoku)
-        if place == 0:
-            return True
-        mogelijk_num = numbers_place(place, sudoku)
-        for number in mogelijk_num:
-            sudoku[place[0]][place[1]] = number
-            checker = dfs(sudoku)
-            if checker:
-                return True
-        if len(mogelijk_num) != 0:
-            sudoku[place[0]][place[1]] = 0
+def processRij(x, num, mogelijkNum):
+    for y in range(0,9):
+        if num in mogelijkNum[x][y]:
+            mogelijkNum[x][y].remove(num)
+    return mogelijkNum
+
+def processColumn(y, num, mogelijkNum):
+    for x in range(0,9):
+        if num in mogelijkNum[x][y]:
+            mogelijkNum[x][y].remove(num)
+    return mogelijkNum
+
+def processBox(x, y, num, mogelijkNum):
+    boxX = int(x/3)
+    boxY = int(y/3)
+    for x in range(3*boxX, 3*boxX + 3):
+        for y in range(3*boxY, 3*boxY + 3):
+            if num in mogelijkNum[x][y]:
+                mogelijkNum[x][y].remove(num)
+    return mogelijkNum
+
+def process_number(place, number, new_possible_list):
+    new_possible_list = processRij(place[1], number, new_possible_list)
+    new_possible_list = processColumn(place[0], number, new_possible_list)
+    new_possible_list = processBox(place[0], place[1], number, new_possible_list)
+    return new_possible_list
+
+def dfs(sudoku, possible_list):
+    new_possible_list = copy.deepcopy(possible_list)
+    print(new_possible_list)
+    place = find_empty(new_possible_list)
+    print(place)
+    if place == 1:
         return False
+    if place == 0:
+        return True
+    mogelijk_num = new_possible_list[place[0]][place[1]]
+    new_possible_list[place[0]][place[1]] = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', ]
+    print(mogelijk_num)
+    for number in mogelijk_num:
+        new_possible_list = process_number(place, number, new_possible_list)
+        for row in sudoku:
+            print(row)
+        print("")
+        checker = dfs(sudoku, new_possible_list)
+        if checker:
+            return True
+    if len(mogelijk_num) != 0:
+        sudoku[place[0]][place[1]] = 0
+    return False
 
-import time
+def start_dfs(sudoku):
+    possible_list = []
+    for y in range(0,9):
+        rij = []
+        for x in range(0,9):
+            if sudoku[y][x] == 0:
+                rij.append([1,2,3,4,5,6,7,8,9])
+            else:
+                rij.append([sudoku[y][x]])
+        possible_list.append(rij)
+    return possible_list
+
 start = time.time()
-for x in range(1,7):
-    sudoku = readSudoku("puzzle"+ str(x) +".sudoku")
-    dfs(sudoku)
+sudoku = readSudoku("puzzle3.sudoku")
+possible_list = start_dfs(sudoku)
+dfs(sudoku, possible_list)
 
-    for row in sudoku:
-        print(row)
-    print("")
+for row in sudoku:
+    print(row)
+print("")
 end = time.time()
 print(end-start)
