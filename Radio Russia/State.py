7849ef1e6dbb1f9ab5ec5dd1c7d6b69c8d8b81aa
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2017-03-09 19:59:22 +0100

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	sudoku.py

--- Radio Russia/State.py
+++ Radio Russia/State.py
@@ -1,1 +1,16 @@
-class state
+class State(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.adjacent_States = []
+
+    def __iadd__(self, new_State):
+        self.adjacent_States.append(new_State)
+
+
+s1 = State("hello")
+s2 = State("bye")
+s1 += s2
+print(s1._name)
+
+
