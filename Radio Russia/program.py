84b1d65143c669aa9b2e95b34bc098e3a285491e
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2017-03-09 20:31:23 +0100

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	sudoku.py

--- Radio Russia/program.py
+++ Radio Russia/program.py
@@ -1,10 +1,11 @@
-import State.py
+import State
 
 def readStates(data):
     states = []
     with open(data, 'r') as info:
         for state in info:
-            x = State.State(state[0])
+            split_state = state.split(',')
+            x = State.State(split_state[0])
             states.append(x)
     states = make_adjecent_states(data, states)
     return states
@@ -13,10 +14,13 @@ def make_adjecent_states(data, states):
     counter = 0
     with open(data, 'r') as info:
         for state in info:
-            adjecent_state in state[1:]:
+            split_state = state.split(',')
+            for adjecent_state in split_state[1:]:
                 states[counter] += adjecent_state
-            counter++
+            counter += 1
     return states
 
 x = readStates("Oekraine.txt")
-
+for state in x:
+    print("state = " + state.name)
+    print(state.adjacent_States)
