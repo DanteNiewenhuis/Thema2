ab7b575695a64534c949565a510d8cc7b7fa512e
Lisa <lisa.pleijt@gmail.com>
2017-03-16 17:17:18 +0100

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	Radio Russia/program.py

--- Radio Russia/Cost.py
+++ Radio Russia/Cost.py
@@ -1,9 +1,3 @@
 class Cost(object):
 
-    def __init__(self, name):
-        self.name = name
-        self.adjacent_States = []
-
-    def __iadd__(self, new_State):
-        self.adjacent_States.append(new_State)
 
