def41451831d8241c7b1fbc714fa0e4f2fe96ac3
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2017-04-06 17:18:15 +0200

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	Radio Russia/main.py

--- Radio Russia/main.py
+++ Radio Russia/main.py
@@ -4,31 +4,48 @@ import analyse
 import hill_solve
 import sim_an_search
 
-loops = 100
-dic = {}
-for x in range (loops):
-    map = readMap.readStates("Oekraine.txt")
-    #print(analyse.analyse_adjacent_states(map))
-    signals = ['zA', 'zB', 'zC', 'zD', 'zE', 'zF', 'zG']
-    costs = {'zA':12, 'zB':26, 'zC':27, 'zD':30, 'zE':37, 'zF':39, 'zG':41}
+with open("analyse_sim.txt", 'w') as text:
+    for n in [10,12,14,16,18,20]:
+        for begin in [1,2,3,4,5]:
+            for end in [0.001,0.003,0.005]:
+                loops = 100
+                dic = {}
+                best_costs = 1000
+                worst_costs = 0
+                for l in range(loops):
+                    map = readMap.readStates("Oekraine.txt")
+                    #print(analyse.analyse_adjacent_states(map))
+                    signals = ['zA', 'zB', 'zC', 'zD', 'zE', 'zF', 'zG']
+                    costs = {'zA':12, 'zB':26, 'zC':27, 'zD':30, 'zE':37, 'zF':39, 'zG':41}
 
-    dfs.dfs(map, signals)
+                    dfs.dfs(map, signals)
 
-    sim_an_search.hill_climber(map, costs, signals)
-    freq = analyse.signal_frequentie(map)
-    best_costs = analyse.get_cost(freq, costs)
-    if best_costs in dic:
-        x = dic[best_costs]
-        dic[best_costs] = x + 1
-    else:
-        dic[best_costs] = 1
-objects = sorted(dic.keys())
-average = 0
-for object in objects:
-    average += object * dic[object]
-average = average / loops
-print(average)
-print(str(objects[0]) + ' = ' + str(dic[objects[0]]))
-analyse.bar_plot(dic)
+                    sim_an_search.hill_climber(map, costs, signals)
+                    freq = analyse.signal_frequentie(map)
+                    hill_costs = analyse.get_cost(freq, costs)
+                    if hill_costs < best_costs:
+                        best_costs = hill_costs
+                    if hill_costs > worst_costs:
+                        worst_costs = hill_costs
+                    if hill_costs in dic:
+                        x = dic[hill_costs]
+                        dic[hill_costs] = x + 1
+                    else:
+                        dic[hill_costs] = 1
+                objects = sorted(dic.keys())
+                average = 0
+                for object in objects:
+                    average += object * dic[object]
+                average = round(average / loops)
+                text.write(str(n) + "," + str(begin) + "," + str(end) +
+                      "," + str(average) + "," + str(best_costs) + "," + str(worst_costs) + "\n")
 
-
+'''
+for state in best_map:
+    print(str(state) + ' = ' + str(state.signal))
+freq = analyse.signal_frequentie(best_map)
+for signal in sorted(freq.keys()):
+    print(str(signal) + ' = ' + str(freq[signal]))
+costs = {'zA':12, 'zB':26, 'zC':27, 'zD':30, 'zE':37, 'zF':39, 'zG':41}
+print(analyse.get_cost(freq, costs))
+'''
--- Radio Russia/sim_an_search.py
+++ Radio Russia/sim_an_search.py
@@ -16,37 +16,45 @@ def swap_state(map, signals):
     swapped_state.signal = new_signal
     return [swapped_state, old_signal]
 
+def sigmodial_temperature(begin, end, n, x):
+    a = (1/(n**2)) * math.log(begin/end)
+    temperature = begin * math.exp(-a*(x**2))
+    return temperature
+
 def hill_climber(map, costs, signals):
-    n = 500
-    begin_temp = 200
-    end_temp = 10
+    n = 10
+    begin_temp = 1
+    end_temp = 0.001
     #plot = []
     freq = analyse.signal_frequentie(map)
     old_costs = analyse.get_cost(freq, costs)
-    for x in range(1,1000):
-        checker = 0
-        a = 1/(n**2) * math.log(begin_temp/end_temp)
-        temperature = begin_temp * math.exp((-a)*(x**2))
-        if temperature <= 0:
-            temperature = 1
-        #plot.append(temperature)
-        swapped_state = swap_state(map, signals)
-        new_freq = analyse.signal_frequentie(map)
-        new_costs = analyse.get_cost(new_freq, costs)
-        improvement = old_costs - new_costs
-        try:
-            chance = math.exp(improvement/temperature)
-            if chance > 1:
-                chance = 1
-        except OverflowError:
-            chance = 0
-        if random.random() < chance:
-            checker = 1
-        if checker == 1:
-            old_costs = new_costs
-        else:
-            revert_changes(swapped_state)
+    for x in range(n):
+        for y in range(100):
+            checker = 0
+            temperature = sigmodial_temperature(begin_temp, end_temp, n, x)
+            if temperature <= 0:
+                temperature = 1
+            swapped_state = swap_state(map, signals)
+            new_freq = analyse.signal_frequentie(map)
+            new_costs = analyse.get_cost(new_freq, costs)
+            improvement = old_costs - new_costs
+            try:
+                chance = math.exp(improvement/temperature)
+                if chance > 1:
+                    chance = 1
+                #if chance > 1:
+                    #print('temp = ' + str(temperature) + ' imp = '+ str(improvement) + ' chance = '+ str(chance))
+                #plot.append(temperature)
+            except OverflowError:
+                chance = 0
+            if random.random() < chance:
+                checker = 1
+            if checker == 1:
+                old_costs = new_costs
+            else:
+                revert_changes(swapped_state)
     '''
+    print(old_costs)
     plt.plot(plot)
     plt.ylabel('temp')
     plt.xlabel('iteraties')
