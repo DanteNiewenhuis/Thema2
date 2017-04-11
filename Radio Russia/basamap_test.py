e1a7e495b560763a3c229beac932f6b52510f335
Alexander Albrecht <afalbrecht@gmail.com>
2017-04-11 11:11:24 +0200

    Randomshit

--- Radio Russia/basamap_test.py
+++ Radio Russia/basamap_test.py
@@ -6,6 +6,18 @@ from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from matplotlib.colors import Normalize
 
+# Ukraine
+m = Basemap(resolution='c',
+            projection='mill')
+
+m.readshapefile('/home/alexander/PycharmProjects/Thema 2/ukraineshapefiles/Ukraine', 'ukraine')
+#m.drawcountries(linewidth=1)
+m.drawcoastlines(linewidth=0.1)
+#m.drawrivers(color='blue')
+#m.drawmapboundary(fill_color='blue')
+
+
+plt.show()
 
 
 
