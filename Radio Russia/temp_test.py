b1cfab6ca8d4b57932edd7ff31d0b013032fd7c5
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2017-05-24 00:46:03 +0200

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	Radio Russia/hill_solve.py

--- Radio Russia/temp_test.py
+++ Radio Russia/temp_test.py
@@ -2,12 +2,12 @@ import analyse
 import readMap
 import sim_an_search
 
-s = '1'
+s = '2'
 signal_costs = analyse.get_cost_scheme(int(s))
 signals = list(signal_costs.keys())
 
 
-for x in range(1,31):
+for x in range(1, 31):
     costs_list = []
     for y in range(20):
         map = readMap.read_complete_map('Russiadfs.txt')
