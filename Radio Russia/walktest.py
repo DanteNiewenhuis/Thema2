4a865ba567281d037c5f366b8f791dc44d227551
Alexander Albrecht <afalbrecht@gmail.com>
2017-05-25 16:12:02 +0200

    Stats af

--- Radio Russia/walktest.py
+++ Radio Russia/walktest.py
@@ -5,12 +5,12 @@ import hill_solve
 
 signals = ['zA', 'zB', 'zC', 'zD', 'zE', 'zF', 'zG']
 
-with open("randomwalk.txt", 'a') as text:
+with open("randomwalk.txt", 'w') as text:
     for c in range(1,14):
         text.write(str(c) + ',')
         map = readMap.read_complete_map('Russiadfs.txt')
         costs = analyse.get_cost_scheme(c)
-        list = hill_solve.random_walker(map, costs, signals, iterations=1000)
+        list = hill_solve.random_walker(map, costs, signals, iterations=10000)
         for x in list:
             text.write(str(x) + ',')
         text.write('\n')
