3011b9b7444f89a56de1553c3c81e7cd1970a6f6
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2017-05-27 12:26:45 +0200

    Merge branch 'master' of https://github.com/DanteNiewenhuis/Thema2
    # Conflicts:
    #	Radio Russia/hill_solve.py

--- Radio Russia/temp_test_2.py
+++ Radio Russia/temp_test_2.py
@@ -2,12 +2,12 @@ import analyse
 import readMap
 import sim_an_search
 
-for s in ['18']:
+for s in ['5']:
     signal_costs = analyse.get_cost_scheme(int(s))
     signals = list(signal_costs.keys())
 
     print('s = ' + s)
-    for x in range(1, 31):
+    for x in range(23, 31):
         print('temperature = ' + str(x))
         costs_list = []
         for y in range(20):
--- Radio Russia/test.py
+++ Radio Russia/test.py
@@ -1,14 +1,20 @@
 import analyse
-import sim_an_search
-import readMap
-import hill_solve
-import dfs
 
-
-map = readMap.read_complete_map('Russiadfs.txt')
-
-signal_costs = analyse.get_cost_scheme(4)
-signals = list(signal_costs.keys())
-#hill_solve.random_walker(map, signal_costs, signals)
-sim_an_search.sim_an(map, signal_costs, signals, 200000, 1, 0.5)
-analyse.print_costs(map, signal_costs)
+for x in range(1, 19):
+    scheme = analyse.get_cost_scheme(x)
+    with open('scheme.txt', 'a') as text:
+        text.write(str(scheme['zA']))
+        text.write(',')
+        text.write(str(scheme['zB']))
+        text.write(',')
+        text.write(str(scheme['zC']))
+        text.write(',')
+        text.write(str(scheme['zD']))
+        text.write(',')
+        text.write(str(scheme['zE']))
+        text.write(',')
+        text.write(str(scheme['zF']))
+        text.write(',')
+        text.write(str(scheme['zG']))
+        text.write(',')
+        text.write('\n')
