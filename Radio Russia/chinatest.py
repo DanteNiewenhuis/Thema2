db3af463986b6f4a0d67d68f03f7f571c034a2c1
Alexander Albrecht <afalbrecht@gmail.com>
2017-05-31 08:06:30 +0200

    Stats af

--- Radio Russia/chinatest.py
+++ Radio Russia/chinatest.py
@@ -8,22 +8,24 @@ cost2 = {'zA':19, 'zB':20, 'zC':21, 'zD':23, 'zE':36, 'zF':37, 'zG':38}
 cost3 = {'zA':16, 'zB':17, 'zC':31, 'zD':33, 'zE':36, 'zF':56, 'zG':57}
 cost4 = {'zA':3, 'zB':34, 'zC':36, 'zD':39, 'zE':41, 'zF':43, 'zG':58}
 costlist = [cost1, cost2, cost3, cost4]
-begintemp = {1:9, 2:8, 3:9, 4:15}
+begintemp = [9, 5, 9, 15]
+maplist = [readMap.read_complete_map('Ukrainedfs.txt'), readMap.read_complete_map('UnitedStatesdfs.txt'), readMap.read_complete_map('Russiadfs.txt')]
 
 
-with open('chinatemp.txt', 'a') as text:
-    c = 1
-    lowest_cost = 1500
-    for y in range(50):
-        map = readMap.read_complete_map('Chinadfs.txt')
-        sim_an_search.sim_an(map, cost1, signals, begin_temp=begintemp[c], end_temp=0.5)
-        freq = analyse.analyse_signal_frequentie(map)
-        costs = analyse.get_cost(freq, cost1)
-        if costs < lowest_cost:
-            lowest_cost = costs
-        print(c, y, lowest_cost)
-    text.write(str(c) + ': ' + str(lowest_cost))
-    text.write('\n')
+with open('temptation.txt', 'a') as text:
+    for m in range(3):
+        for c in range(4):
+            lowest_cost = 2000
+            for y in range(20):
+                map = maplist[m]
+                sim_an_search.sim_an(map, costlist[c], signals, begin_temp=begintemp[c], end_temp=0.5)
+                freq = analyse.analyse_signal_frequentie(map)
+                costs = analyse.get_cost(freq, costlist[c])
+                if costs < lowest_cost:
+                    lowest_cost = costs
+                print(m, c + 1, y, lowest_cost)
+            text.write(str(m) + str(c + 1) + ': ' + str(lowest_cost))
+            text.write('\n')
 
 
 
--- Radio Russia/dfs_test.py
+++ Radio Russia/dfs_test.py
@@ -2,13 +2,13 @@ import analyse
 import readMap
 import dfs
 
-map = readMap.read_empty_map('NewChina.txt')
+map = readMap.read_empty_map('New_Oekraine')
 signal_costs = analyse.get_cost_scheme(4)
 signals = list(signal_costs.keys())
 dfs.dfs(map, signals)
 analyse.print_costs(map, signal_costs)
 
-with open('Chinadfs.txt', 'w') as text:
+with open('Ukrainedfs.txt', 'w') as text:
     for state in map:
         text.write(str(state.name))
         text.write(',')
